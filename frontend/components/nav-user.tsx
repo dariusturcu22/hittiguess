f023fa0a0528d0f8d37fa1265949bec75d41f26a
dariusturcu22 <protdm22@gmail.com>
2026-08-21 22:12:27 +0300

    Remove the dead Account menu item, no account page exists to link it to

--- frontend/components/nav-user.tsx
+++ frontend/components/nav-user.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import {
   IconDotsVertical,
   IconLogout,
-  IconUserCircle,
   IconMoon,
   IconSun,
 } from "@tabler/icons-react";
@@ -122,10 +121,6 @@ export function NavUser() {
                     <span>Loading...</span>
                   </>
                 )}
-              </DropdownMenuItem>
-              <DropdownMenuItem className="cursor-pointer">
-                <IconUserCircle />
-                Account
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
